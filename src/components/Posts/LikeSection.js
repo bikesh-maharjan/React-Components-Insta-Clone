39bf222b09605f1da0753419f21ae812ba859fe2
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-07-14 13:06:28 -0700

    Like Functionality Added

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -2,29 +2,29 @@
 // console.log your props and see what to use so this component will display the correct likes count.
 // You will also need to add an onClick event handler that utilizes `likePost` to increase the likes count.
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 
-const LikeSection = props => {
+const LikeSection = (props) => {
   // Make sure the parent of LikeSection is passing the right props!
-  const { likePost, likeCount } = props
+  const { likePost, likeCount } = props;
 
   return (
     <div>
-      <div
-        className="like-section"
-        key="likes-icons-container"
-      >
+      <div className="like-section" key="likes-icons-container">
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon
+            icon={faHeart}
+            onClick={() => likePost(likeCount + 1)}
+          />
         </div>
         <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className="like-number">100 likes</p>
+      <p className="like-number">{props.likeCount}</p>
     </div>
-  )
+  );
 };
 
 export default LikeSection;
